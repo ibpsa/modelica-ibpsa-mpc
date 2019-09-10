fb30ee53d2458c7a6e5d641911cfcc8014ba456b
Filip Jorissen <filip.jorissen@kuleuven.be>
2019-09-10 10:26:32 +0200

    added merge script

---
+++ IbpsaMpc/Resources/Scripts/merge.py
@@ -0,0 +1,26 @@
+import buildingspy.development.merger as m
+import os
+modelicapath = os.environ["MODELICAPATH"]
+ibpsa_dir = os.path.join(modelicapath, "IBPSA")
+dest_dir = os.path.join(modelicapath, "IbpsaMpc")
+mer = m.IBPSA(ibpsa_dir, dest_dir) # doctest: +SKIP
+
+files={
+"IBPSA.Fluid.Sources.Boundary_pT": {},
+"IBPSA.Fluid.Sources.Boundary_ph": {},
+"IBPSA.Fluid.Sources.MassFlowSource_T": {},
+"IBPSA.Fluid.Sources.MassFlowSource_h": {},
+"IBPSA.Fluid.Sources.PropertySource_T": {},
+"IBPSA.Fluid.Sources.PropertySource_h": {},
+"IBPSA.Fluid.Sources.BaseClasses.PartialPropertySource": {},
+"IBPSA.Fluid.Sources.BaseClasses.PartialSource": {},
+"IBPSA.Fluid.Sources.BaseClasses.PartialSource_Xi_C": {},
+"IBPSA.Fluid.Interfaces.PartialTwoPort": {},
+"IBPSA.Media.Air": {},
+# "": {},
+# "": {},
+
+		}
+
+
+mer.mergeIbpsaMpc(files)
